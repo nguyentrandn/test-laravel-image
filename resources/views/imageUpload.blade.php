889342282e58a31d3d5d75612175ebd641a2693d
Nguyen <nguyentrandn33@gmail.com>
2022-09-30 14:44:59 +0700

    add asset to href

--- resources/views/imageUpload.blade.php
+++ resources/views/imageUpload.blade.php
@@ -15,7 +15,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
 
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
@@ -68,7 +68,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="btn-download"><a href="{{'images/'.$img->name}}" target="_blank" download><i class="fa-solid fa-circle-down"></i></a></div>
+                <div class="btn-download"><a href="{{asset("'images/'.$img->name")}}" target="_blank" download><i class="fa-solid fa-circle-down"></i></a></div>
             </div>
             @endforeach
         </div>
